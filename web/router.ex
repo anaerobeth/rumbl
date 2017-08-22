755a47d0156b3016de7d37d55b148620ea4f6fdf
Beth Tenorio <betsky98@gmail.com>
2017-08-22 10:23:16 -0400

    add support for videos

--- web/router.ex
+++ web/router.ex
@@ -19,9 +19,15 @@ defmodule Rumbl.Router do
 
     resources "/users", UserController, only: [:index, :show, :new, :create]
     resources "/sessions", SessionController, only: [:new, :create, :delete]
+    resources "/videos", VideoController
     get "/", PageController, :index
   end
 
+  scope "/manage", Rumbl do
+    pipe_through [:browser, :authenticate_user]
+
+    resources "/videos", VideoController
+  end
   # Other scopes may use custom stacks.
   # scope "/api", Rumbl do
   #   pipe_through :api
